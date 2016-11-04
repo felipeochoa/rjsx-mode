5e038d5cf5d64b2aba2e8137e405136e6a47850f
Felipe Ochoa <felipe@fov.space>
2016-11-04 15:02:14 +0100

    marketing

--- rjsx-mode.el
+++ rjsx-mode.el
@@ -9,8 +9,21 @@
 ;; Keywords: languages
 
 ;;; Commentary:
-;; Defines a major mode `rjsx-mode' that swaps out js2-mode's
-;; XML parsing (made for E4X) with a JSX parser
+;; Defines a major mode `rjsx-mode' based on `js2-mode' for editing
+;; JSX files.  `rjsx-mode' extends the parser in `js2-mode' to support
+;; the full JSX syntax.  This means you get all of the `js2' features
+;; plus proper syntax checking and highlighting of JSX code blocks.
+;;
+;; Some features that this mode adds to js2:
+;;
+;; - Highlighting JSX tag names and attributes (using the rjsx-tag and
+;;   rjsx-attr faces)
+;; - Parsing the spread operator {...otherProps}
+;; - Parsing && and || in child expressions {cond && <BigComponent/>}
+;; - Parsing ternary expressions {toggle ? <ToggleOn /> : <ToggleOff />}
+;;
+;; Additionally, since rjsx-mode extends the js2 AST, utilities using
+;; the parse tree gain access to the JSX structure.
 
 ;;; Code:
 
